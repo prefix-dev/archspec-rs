0864ab17f7127ebf3565c77e1818baac46fb9db9
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-04-05 14:53:22 +0300

    style: update print messages

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,11 +2,11 @@ fn main() -> Result<(), archspec::cpu::UnsupportedMicroarchitecture> {
     let architecture = archspec::cpu::host()?;
 
     println!("Current CPU architecture:");
-    println!("  Name: {}", architecture.name());
-    println!("  Vendor: {}", architecture.vendor());
-    println!("  Generation: {}", architecture.generation());
-    println!("  Family Name: {}", architecture.family().name());
-    println!("  Features: {:?}", architecture.all_features());
+    println!("- Name: {}", architecture.name());
+    println!("- Vendor: {}", architecture.vendor());
+    println!("- Generation: {}", architecture.generation());
+    println!("- Family Name: {}", architecture.family().name());
+    println!("- Features: {:?}", architecture.all_features());
 
     Ok(())
 }
